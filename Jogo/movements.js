f8171bd040eb3e0c26ffeab270602131713aafea
LuizMFL <luiz.m.franca1@gmail.com>
2023-07-12 18:57:48 -0300

    Person with move

--- Jogo/movements.js
+++
@@ -1,7 +0,0 @@
-function setupInputs() {
-    document.addEventListener("keydown", function (event) {
-        if (event.key === "w" || event.key === "ArrowUp") {
-            upkey = true;
-        }
-    });
-}
